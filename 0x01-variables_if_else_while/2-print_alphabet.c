c1dcea1f67da14ed8421f5488c44cd08eaf2a413
Nelson <ominimarketme@gmail.com>
2022-07-01 10:24:12 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - display a - z
+ * Return: 0 if there is no hikky
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
