75818bde7a2e3fbaa678be10fa0e11db2cf65ab4
Nelson <ominimarketme@gmail.com>
2022-07-01 14:40:52 -0700

    fixed for the last time

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,19 +17,18 @@ int main(void)
 			{
 				if (i == j || j == k || i == k)
 					continue;
-					putchar(i);
-					putchar(j);
-					putchar(k);
-
-					if (i == 55 && j == 56 && k == 57)
-					{
-						break;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 	putchar('\n');
 	return (0);
