38e3c536acc32efc7f3ed78ca8262fe2b1576a56
Nelson <ominimarketme@gmail.com>
2022-07-07 14:27:47 -0700

    this will print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ *
+ * Description: used the % to get the remainder
+ * and then combine the value of the division with the remainder
+ * to print the two digit numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+				_putchar(num / 10 + '0');
+
+			_putchar(num % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
