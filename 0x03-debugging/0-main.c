4fe161762f5cee259c9a4d4583961d537681985d
Nelson <ominimarketme@gmail.com>
2022-07-06 18:47:33 -0700

    making sure my line in 16

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* main - prints if integer is positive or negative
-*
-* Return: 0
-*/
-
+ * main - tests function for is positive or negative
+ * to check if i=0 works correctly.
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	positive_or_negative(i);
 
 	return (0);
