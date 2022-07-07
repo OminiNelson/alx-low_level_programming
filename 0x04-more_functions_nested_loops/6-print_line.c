104805762881d8071872a17b075502f7480fccf4
Nelson <ominimarketme@gmail.com>
2022-07-07 14:34:55 -0700

    this will print an _ as a line, and it will print it n number of times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line n number of times
+ * using _ as a line
+ * @n: Number of lines wanted
+ *
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+
+		_putchar('\n');
+	}
+}
