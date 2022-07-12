6cfb2365f200cf0a72d077371ebd5714949a263d
Nelson <ominimarketme@gmail.com>
2022-07-12 12:50:55 -0700

    printing a string from the end

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - this function will print a string in reverse
+ * @s: is a string parameter
+ *
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int tmp = 0;
+
+	while (s[temp] != 0)
+	{
+		tmp++;
+	}
+	while (tmp > 0)
+	{
+		tmp--;
+		_putchar(s[tmp]);
+	}
+	_putchar('\n');
+}
